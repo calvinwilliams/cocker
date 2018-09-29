200583d2aa3d2976bad925e646bd295727a89325
calvinwilliams <calvinwilliams@163.com>
2018-09-29 16:47:04 +0800

    UPDATE TO V0.6.0

--- src/util/version.c
+++ src/util/version.c
@@ -1,5 +1,5 @@
 #include "cocker_util.h"
 
-char	_COCKER_VERSION_0_5_0[] = "0.5.0" ;
-char	*_COCKER_VERSION = _COCKER_VERSION_0_5_0 ;
+char	_COCKER_VERSION_0_6_0[] = "0.6.0" ;
+char	*_COCKER_VERSION = _COCKER_VERSION_0_6_0 ;
 
